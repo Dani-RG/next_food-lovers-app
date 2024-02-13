2ee345772ffd65b9dddf2f86ce412f3a06a382da
Daniel-RG <21danielrondong@gmail.com>
2024-02-13 08:08:00 +0100

    add error handling and not found alert

---
+++ app/meals/loading-out.js
@@ -0,0 +1,6 @@
+//this file is not getting used, but we keep it as an option
+import classes from "./loading.module.css";
+
+export default function MealsLoadingPage() {
+  return <p className={classes.loading}>Fetching meals...</p>;
+}
